fc2d70e875acadabad526f7030bcc3484b82b100
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-02-23 01:12:50 +0530

    Add files via upload

---
+++ guessthenum.py
@@ -0,0 +1,25 @@
+# here we create game - guess the number
+
+n=5 #total number of guess
+
+no_of_guess=1 #start with 1
+print("Number of guess is 5")
+
+# while we use
+while(no_of_guess<=5):
+    guess_no=int(input("Guess the number :"))
+    if guess_no<5: #condition1
+        print("its lower number")
+    elif guess_no>5:#condition2
+        print("its higher number")
+    else:
+        print("you won") #condition3
+        print((no_of_guess,"no og guess you took"))
+        break
+    print(5-no_of_guess,"number of guess left")
+    no_of_guess+=1 #here we count number of guess
+
+if no_of_guess>5:
+    print("game over")    
+ 
+
