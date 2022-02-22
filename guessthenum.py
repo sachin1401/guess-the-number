9b747ff7036409b3a240d24c15acce40af5d299c
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-02-23 01:15:00 +0530

    Update guessthenum.py

--- guessthenum.py
+++ guessthenum.py
@@ -5,15 +5,15 @@ n=5 #we have to find 5 in limit of guesses
 no_of_guess=1 #start with 1
 print("Number of guess is 5")
 
-# while we use
+# while loop we use
 while(no_of_guess<=5):
     guess_no=int(input("Guess the number :"))
-    if guess_no<5: #condition1
+    if guess_no<5: #condition 1
         print("its lower number")
-    elif guess_no>5:#condition2
+    elif guess_no>5:#condition 2
         print("its higher number")
     else:
-        print("you won") #condition3
+        print("you won") #condition 3
         print((no_of_guess,"no og guess you took"))
         break
     print(5-no_of_guess,"number of guess left")
